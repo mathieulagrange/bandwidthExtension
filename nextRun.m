0e7968505ebeaa540409b4f8c5c763a65d5f02cf
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2019-10-18 09:12:15 +0200

    nextrun

---
+++ nextRun.m
@@ -0,0 +1,11 @@
+% last auto
+bandwidthExtension('do', 4, 'mask', {{[3], 1, [6], 3, 2, 2, [3], 2, 0, [2], 0 , 0, 0, 0, 1}}, 'host', 2.1, 'parallel', -8)
+bandwidthExtension('do', 4, 'mask', {{[2], 1, [6], 3, 2, 2, [3], 2, 0, [2], 0 , 0, 0, 0, 1}}, 'host', 1.2, 'parallel', -6)
+
+% baselines
+% bandwidthExtension('do', 4, 'mask', {{[3], 1, [2 3 4], 3, 2, 2, [3], 2, 0, [2], 0 , 0, 0, 0, 1}}, 'host', 2.1, 'parallel', -8)
+% bandwidthExtension('do', 4, 'mask', {{[2], 1, [2 3 4], 3, 2, 2, [3], 2, 0, [2], 0 , 0, 0, 0, 1}}, 'host', 1.2, 'parallel', -6)
+
+% all on oracle phases
+% bandwidthExtension('do', 4, 'mask', {{[3], 1, [1:6], 3, 2, 2, [3], 2, 0, [4], 0 , 0, 0, 0, 1}}, 'host', 2.1, 'parallel', -8)
+% bandwidthExtension('do', 4, 'mask', {{[2], 1, [1:6], 3, 2, 2, [3], 2, 0, [4], 0 , 0, 0, 0, 1}}, 'host', 1.2, 'parallel', -6)
